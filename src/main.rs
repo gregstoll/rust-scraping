af04de68b12db0bcc945891f1ba912f753859bd4
gregstoll <greg@gregstoll.com>
2022-06-05 21:46:55 -0500

    finish parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use reqwest::Error;
 use scraper::{Html, Selector};
 use lazy_static::lazy_static;
@@ -13,15 +15,33 @@ lazy_static! {
     static ref TD: Selector = make_selector("td");
 }
 
-// Holds the column indices where the values for number of people
-// still alive are held.
-struct ColumnIndices {
-    row_number: u32,
-    male: u32,
-    female: u32
+#[derive(Clone, Debug)]
+struct SurvivorsAtAgeTable {
+    pub male: Vec<f32>,
+    pub female: Vec<f32>
 }
 
-fn parse_page(year: u32) -> Result<(), Error> {
+// Holds the column indices where the values for number of people
+// still alive are held.
+#[derive(Clone, Copy, Debug)]
+struct ColumnIndices {
+    pub row_number: usize,
+    pub male: usize,
+    pub female: usize
+}
+
+impl ColumnIndices {
+    pub fn max_index(&self) -> usize {
+        *([self.row_number, self.male, self.female].iter().max().unwrap())
+    }
+}
+
+fn get_text(cell: &scraper::ElementRef) -> String {
+    // The DOM allows multiple text nodes of an element, so join them all together.
+    cell.text().collect::<Vec<_>>().join("").trim().replace(",", "")
+}
+
+fn parse_page(year: u32) -> Result<SurvivorsAtAgeTable, Error> {
     println!("Parsing year {}", year);
     let url = format!("https://www.ssa.gov/oact/NOTES/as120/LifeTables_Tbl_7_{}.html", year);
     let body = reqwest::blocking::get(url)?
@@ -33,50 +53,92 @@ fn parse_page(year: u32) -> Result<(), Error> {
         table.select(&TR).count()
     }).expect("No tables found in document?");
 
+    // TODO - better name
+    let mut male_values = Vec::<f32>::new();
+    let mut female_values = Vec::<f32>::new();
     // Find the columns we want
-    let column_indices: Option<ColumnIndices> = None;
+    let mut column_indices: Option<ColumnIndices> = None;
+    let mut next_row_number: u32 = 0;
     for row in main_table.select(&TR) {
-        let entries = row.select(&TD);
-        //TODO
-        /*if entries.clone().count() < 4 {
-            // row is too short; skip it
-            continue;
-        }*/
+        // Need to collect this into a Vec<> because we're going to be iterating over it
+        // multiple times.
+        let entries = row.select(&TD).collect::<Vec<_>>();
         if column_indices.is_none() {
-            let mut row_number_index: Option<u32> = None;
-            let mut male_index: Option<u32> = None;
-            let mut female_index: Option<u32> = None;
+            let mut row_number_index: Option<usize> = None;
+            let mut male_index: Option<usize> = None;
+            let mut female_index: Option<usize> = None;
             // look for values of "0" (for the row number) and "100000"
-            for (column_index, cell) in entries.into_iter().enumerate() {
+            for (column_index, cell) in entries.iter().enumerate() {
                 // The DOM allows multiple text nodes of an element, so join them all together.
-                let text: String = cell.text().collect::<Vec<_>>().join("");
-                let text: &str = text.trim();
+                let text: String = get_text(cell);
                 if text == "0" {
                     // Only want the first column that has a value of "0"
-                    row_number_index = row_number_index.or(Some(column_index as u32));
+                    row_number_index = row_number_index.or(Some(column_index));
                 } else if text == "100000" {
                     // male columns are first
                     if male_index.is_none() {
-                        male_index = Some(column_index as u32);
+                        male_index = Some(column_index);
                     }
                     else if female_index.is_none() {
-                        female_index = Some(column_index as u32);
+                        female_index = Some(column_index);
                     }
                     else {
                         panic!("Found too many columns with text \"100000\"!");
                     }
                 }
             }
+            assert_eq!(male_index.is_some(), female_index.is_some(), "Found male column but not female?");
+            if male_index.is_some() {
+                assert!(row_number_index.is_some(), "Found male column but not row number?");
+                column_indices = Some(ColumnIndices {
+                    row_number: row_number_index.unwrap(),
+                    male: male_index.unwrap(),
+                    female: female_index.unwrap()
+                });
+            }
+        }
+        if let Some(column_indices) = column_indices {
+            if entries.len() < column_indices.max_index() {
+                // Too few columns, this isn't a real row
+                continue
+            }
+            let row_number_text = get_text(&entries[column_indices.row_number]);
+            if row_number_text.parse::<u32>().and_then(|x| Ok(x == next_row_number)) == Ok(true) {
+                next_row_number += 1;
+                let male_value = get_text(&entries[column_indices.male]).parse::<u32>();
+                let male_value = male_value.expect("Couldn't parse value in male cell");
+                // TODO document this
+                let male_value = male_value as f32 / 100000_f32;
+                assert!(male_value <= 1.0, "male value is out of range");
+                if male_values.len() > 0 {
+                    assert!(*male_values.last().unwrap() >= male_value, "male values are not decreasing");
+                }
+                male_values.push(male_value);
+
+                let female_value = get_text(&entries[column_indices.female]).parse::<u32>();
+                let female_value = female_value.expect("Couldn't parse value in female cell");
+                let female_value = female_value as f32 / 100000_f32;
+                assert!(female_value <= 1.0, "female value is out of range");
+                if female_values.len() > 0 {
+                    assert!(*female_values.last().unwrap() >= female_value, "female values are not decreasing");
+                }
+                female_values.push(female_value);
+            }
         }
     }
-    Ok(())
+    assert_eq!(male_values.len(), female_values.len());
+    assert!(male_values.len() > 50);
+
+    Ok(SurvivorsAtAgeTable {
+        male: male_values,
+        female: female_values
+    })
 }
 
 fn main() -> Result<(), Error> {
-    //TODO
-    //for year in (1900..=2100).step_by(10) {
-    for year in (1900..=1901).step_by(10) {
-        parse_page(year)?;
+    let mut all_data: HashMap<u32, SurvivorsAtAgeTable> = HashMap::new();
+    for year in (1900..=2100).step_by(10) {
+        all_data.insert(year, parse_page(year)?);
     }
     return Ok(());
 }
